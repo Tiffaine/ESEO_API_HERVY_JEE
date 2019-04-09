504d72ca4e351682eb7a19cb6561241bbfb486e7
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-04-09 11:09:30 +0200

    Jusqu'à l'affichage de la distance OK. Calcul sur repère plan.

---
+++ ESEO_API_JEE/src/fr/eseo/twic/model/CalculServlet.java
@@ -0,0 +1,60 @@
+package fr.eseo.twic.model;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CalculServlet
+ */
+@WebServlet("/CalculServlet")
+public class CalculServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalculServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		String lat1 = request.getParameter("lat1");
+		String lng1 = request.getParameter("lng1");
+		String lat2 = request.getParameter("lat2");
+		String lng2 = request.getParameter("lng2");
+		double lat1Double = Double.parseDouble(lat1);
+		double lng1Double = Double.parseDouble(lng1);
+		double lat2Double = Double.parseDouble(lat2);
+		double lng2Double = Double.parseDouble(lng2);
+	
+		//calcul sur un repere plan 
+		double distance = Math.sqrt((lng2Double-lng1Double)*(lng2Double-lng1Double)+(lat2Double-lat1Double)*(lat2Double-lat1Double));
+
+		RequestDispatcher dispat = request.getRequestDispatcher("affichageDistance.jsp"); 
+		HttpSession session = request.getSession(); 
+		session.setAttribute("distance", distance);
+		dispat.forward(request, response);
+	}
+
+}
--- ESEO_API_JEE/src/fr/eseo/twic/model/CalculerDistanceServ.java
+++ ESEO_API_JEE/src/fr/eseo/twic/model/CalculerDistanceServ.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class CalculerDistanceServ
@@ -28,32 +29,17 @@ public class CalculerDistanceServ extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String ville1 = request.getParameter("selectVille1");
 		String ville2 = request.getParameter("selectVille2");
-		String ville3 = request.getParameter("selectVille3");
 
-		System.out.println("ville1"+ville1);
-		System.out.println("ville2"+ville2);
-		System.out.println("ville3"+ville3);
-		RequestDispatcher dispat = request.getRequestDispatcher("affichageData.jsp");
+		System.out.println("ville1 "+ville1);
+		System.out.println("ville2 "+ville2);
+		HttpSession session = request.getSession(); 
+		session.setAttribute("ville1", ville1);
+		session.setAttribute("ville2", ville2);
 		
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher dispat = request.getRequestDispatcher("affichageData.jsp");
+		dispat.forward(request, response);
 	}
 
 }
